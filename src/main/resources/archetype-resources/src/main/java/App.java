f0dd298400bad45dd5d1be1b78403204302cbbed
yumo <yumo@qq.com>
2026-05-07 11:46:43 +0800

    Push commit

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println( "Hello World! main Test" )
         System.out.println( "Hello World Lastone!" );
         System.out.println( "Hello World One More Times!" );
-        System.out.println( "Hello World Hot-fix Test!" )
+        System.out.println( "Hello World Hot-fix Test!" );
+        System.out.println( "Hello World Push Test!" )
     }
 }
